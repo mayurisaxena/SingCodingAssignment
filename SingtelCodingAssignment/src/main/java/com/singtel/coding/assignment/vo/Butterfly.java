40ca2abc26bf26ed0866b21acec5d32dc5ff756f
Mayuri Saxena <mayurisaxena9@gmail.com>
2019-07-21 03:50:44 +0800

    Question D: Butterfly

---
+++ SingtelCodingAssignment/src/main/java/com/singtel/coding/assignment/vo/Butterfly.java
@@ -0,0 +1,8 @@
+package com.singtel.coding.assignment.vo;
+
+public class Butterfly extends Caterpillar {
+	public Butterfly() {
+		this.setCanFly(true);
+		this.setCanSing(false);
+	}
+}
---
+++ SingtelCodingAssignment/src/main/java/com/singtel/coding/assignment/vo/Caterpillar.java
@@ -0,0 +1,10 @@
+package com.singtel.coding.assignment.vo;
+
+public class Caterpillar extends Animal{
+
+	public Caterpillar() {
+		this.setCanFly(false);
+		this.setCanWalk(true);
+	}
+
+}
